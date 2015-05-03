f2adc967b98e7d863685deb2c26056202a87cd2c
JavierKazag <javier@javierandre.com>
2015-05-03 14:12:03 -0700

    Version 0.3

---
+++ Adventure/src/myworldsim/CommandInterpreterModel.java
@@ -0,0 +1,358 @@
+package myworldsim;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CommandInterpreterModel
+{
+	/*
+	 * Class Instance Variables
+	 * 
+	 */
+	
+	private CommandTree<CommandModel> 	commandTree;
+	private boolean 					executionStatus;
+	
+	/*
+	 * Class Constants
+	 * 
+	 */
+	
+	private static final String			RESERVED_WORD 		= "_reserved";
+	public static final String			COMMAND_NOT_FOUND 	= "I don't now how to do that.";
+	
+	/*
+	 * Constructor
+	 * 
+	 */
+	
+	public CommandInterpreterModel() {
+		commandTree = new CommandTree<CommandModel>();
+	}
+	
+	/*
+	 * Getters and Setters
+	 * 
+	 */
+	
+	public boolean getStatus() {
+		return executionStatus;
+	}
+	
+	/*
+	 * buildBinarySearchCommandTree Methods
+	 * 
+	 */
+	
+	public boolean buildBinarySearchCommandTree(List<CommandModel> commandList) {
+		
+		boolean built = false;
+			
+		if (commandList.size() > 0) {
+
+			for (CommandModel commandModel : commandList) {				
+				commandTree.insert(commandModel);
+			}
+		}
+
+		return built;
+	}
+	
+	/*
+	 * Get Commands Methods
+	 * 
+	 */
+	
+	public List<CommandModel> getCommands() {
+		return commandTree.preOrder();
+	}
+	
+	public List<CommandModel> getCommandsInOrder() {
+		return commandTree.inOrder();
+	}
+	
+	public List<CommandModel> getPrimaryCommands() {
+		
+		List<CommandModel> commandModelList 	= getCommandsInOrder();
+		List<CommandModel> primaryCommandList 	= new LinkedList<CommandModel>();
+		
+		for (CommandModel commandModel: commandModelList) {
+			
+			if (commandModel.getPrimaryCommandId() == 0) {
+				primaryCommandList.add(commandModel);
+			}
+		}
+		
+		return primaryCommandList;
+	}
+	
+	public List<CommandModel> getSynonymCommands() {
+		
+		List<CommandModel> commandModelList 	= getCommandsInOrder();
+		List<CommandModel> synonymCommandList 	= new LinkedList<CommandModel>();
+		
+		for (CommandModel commandModel: commandModelList) {
+			
+			if (commandModel.getPrimaryCommandId() > 0) {
+				synonymCommandList.add(commandModel);
+			}
+		}
+		
+		return synonymCommandList;
+	}
+
+	/*
+	 * findCommand Methods
+	 * 
+	 */
+	
+	public CommandModel findCommand(String command) {
+		
+		CommandModel commandModel = new CommandModel();
+		commandModel.setCommandName(command);
+		
+		CommandTreeNode<CommandModel> commandTreeNode = commandTree.find(commandModel);
+
+		// Check if the command was found
+		if (commandTreeNode.getData() != CommandTreeNode.NOT_FOUND) {
+			
+			// Check if it is a Synonym Command
+			commandModel = (CommandModel)commandTreeNode.getData();
+			
+			if (commandModel.getPrimaryCommandId() > 0) {
+				
+				// Find the Primary Command. Caller is responsible for testing whether or not it was found (call getCommandId()).
+				commandModel = findCommand(commandModel.getPrimaryCommandName());
+			}
+		}
+		
+		return commandModel;
+	}
+	
+	/*
+	 * Commands
+	 * 
+	 */
+	
+	public String executeCommand(String command) {
+
+		String	executionMessage = "";
+		boolean executed 		 = false;
+		
+		// Find the command and get its command ID
+		CommandModel commandModel = new CommandModel();
+		commandModel.setCommandName(command);
+		
+		CommandTreeNode<CommandModel> commandTreeNode = commandTree.find(commandModel);
+
+		// Check if the command was found
+		if (commandTreeNode.getData() != CommandTreeNode.NOT_FOUND) {
+			
+			commandModel = (CommandModel) commandTreeNode.getData();
+		
+			// String parameterl
+			Class<?>[] paramString = new Class[1];
+			paramString[0] = String.class;
+			
+			try {
+				Class<?> cls = Class.forName("myworldsim.CommandInterpreterModel");
+				Object obj = cls.newInstance();
+		 
+				// Call the Command method, pass a String param 
+				Method method = cls.getDeclaredMethod(command, paramString);
+				method.invoke(obj, new String(command));
+
+				executionMessage = commandModel.getCommandText();
+				executed = true;
+			}
+			catch(ClassNotFoundException e){
+				System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+			}
+			catch(IllegalAccessException e){
+				System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+			}
+			catch(InstantiationException e){
+				System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+			}
+			catch(NoSuchMethodException e){
+				
+				// Check for a reserved word Command
+				try {
+					Class<?> cls = Class.forName("myworldsim.CommandInterpreterModel");
+					Object obj = cls.newInstance();
+			 
+					// Call the Command method, pass a String param 
+					Method method = cls.getDeclaredMethod(command + RESERVED_WORD, paramString);
+					method.invoke(obj, new String(command));
+
+					executionMessage = commandModel.getCommandText();
+					executed 		 = true;
+				}
+				catch(ClassNotFoundException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(IllegalAccessException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(InstantiationException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(NoSuchMethodException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Method " + command + RESERVED_WORD + " in class myworldsim.CommandInterpreterModel not found.");
+					executionMessage = COMMAND_NOT_FOUND;
+				}
+				catch(InvocationTargetException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Method " + command + RESERVED_WORD + " in class myworldsim.CommandInterpreterModel not found.");
+					executionMessage = COMMAND_NOT_FOUND;
+				}
+			}
+			catch(InvocationTargetException e){
+				
+				// Check for a reserved word Command
+				try {
+					Class<?> cls = Class.forName("myworldsim.CommandInterpreterModel");
+					Object obj = cls.newInstance();
+			 
+					// Call the Command method, pass a String param 
+					Method method = cls.getDeclaredMethod(command + RESERVED_WORD, paramString);
+					method.invoke(obj, new String(command));
+
+					executionMessage = commandModel.getCommandText();
+					executed 		 = true;
+				}
+				catch(ClassNotFoundException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(IllegalAccessException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(InstantiationException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Class myworldsim.CommandInterpreterModel not found.");
+				}
+				catch(NoSuchMethodException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Method " + command + RESERVED_WORD + " in class myworldsim.CommandInterpreterModel not found.");
+					executionMessage = COMMAND_NOT_FOUND;
+				}
+				catch(InvocationTargetException ex){
+					System.out.println("CommandInterpreterModel::executeCommand(): Method " + command + RESERVED_WORD + " in class myworldsim.CommandInterpreterModel not found.");
+					executionMessage = COMMAND_NOT_FOUND;
+				}
+			}
+		}
+		
+		executionStatus = executed;
+		return executionMessage;
+	}
+		
+	private void move(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void throw_reserved(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void shoot(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void get(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void open(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void close(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void sit(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void stand(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void hit(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void break_reserved(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void light(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void cut(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void cook(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void kill(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void eat(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void launch(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void pickup(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void take(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void shutoff(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void tighten(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void turnon(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+	
+	private void throwup(String command) {
+		
+		//System.out.println("Let's " + command + "!");
+	}
+}
---
+++ Adventure/src/unittests/TestCommandInterpreter.java
@@ -0,0 +1,186 @@
+package unittests;
+
+import java.lang.reflect.Method;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import myworldsim.CommandInterpreterModel;
+import myworldsim.CommandModel;
+import myworldsim.Controller;
+
+public class TestCommandInterpreter {
+	
+private Controller controller;
+	
+	public TestCommandInterpreter() {
+		this.controller = new Controller();
+	}
+
+	public void menu() {
+		
+		int option 		 = 0;
+		Scanner keyboard = new Scanner(System.in);
+		
+		while (option != 9) {
+			
+			System.out.println("\n" + "Command Interpreter Tests" + "\n");
+			
+			System.out.println("1. Test a Primary Command");
+			System.out.println("2. Test all Primary Commands");
+			System.out.println("3. Test all Primary Commands by Reflection");
+			System.out.println("4. Test a Synonym Command");
+			System.out.println("5. Test all Synonym Commands");
+			System.out.println("9. Exit");
+	
+			try {
+				System.out.print("\nEnter your Menu choice: ");
+				option = keyboard.nextInt();
+				System.out.println();
+			}
+			catch (InputMismatchException e) {
+				keyboard.nextLine();
+			}
+			
+			switch (option) {
+			
+				case 1:
+				{
+					executePrimaryCommand();
+					break;
+				}
+				case 2:
+				{
+					executeAllPrimaryCommands();
+					break;
+				}
+				case 3:
+				{
+					executeAllPrimaryCommandsByReflection();
+					break;
+				}
+				case 4:
+				{
+					executeSynonymCommand();
+					break;
+				}
+				case 5:
+				{
+					executeAllSynonymCommands();
+					break;
+				}
+				case 9:
+				{
+					System.out.println("All Command Interpreter Tests performed.\n");
+					break;
+				}
+				default:
+				{
+					System.out.println("Invalid Menu Option.\n");
+					break;
+				}
+			}
+		}
+	}
+	
+	/*
+	 * Commands
+	 * 
+	 */
+	
+	private void executePrimaryCommand() {
+		
+		System.out.println("Command Interpreter Primary Command Test: Move" + "\n");
+		
+		// Commands needs to be all lowercase in order to match the method names
+		String executionMessage = controller.executeCommand("move");
+		
+		System.out.println("\n" + executionMessage + "\n");
+		
+		System.out.println("Command Interpreter Primary Command Test: List" + "\n");
+		
+		// Commands needs to be all lowercase in order to match the method names
+		executionMessage = controller.executeCommand("list");
+		
+		System.out.println(executionMessage);
+	}
+	
+	private void executeAllPrimaryCommands() {
+
+		List<CommandModel> primaryCommandList = controller.getPrimaryCommands();
+		
+		System.out.println("Command Interpreter All Primary Commands Test" + "\n");
+		
+		for (CommandModel commandModel : primaryCommandList) {
+
+			// Commands needs to be all lowercase in order to match the method names
+			String executionMessage = controller.executeCommand(commandModel.getCommandName());
+			
+			System.out.println("\n" + executionMessage + "\n");
+        }
+	}
+	
+	private void executeAllPrimaryCommandsByReflection() {
+		
+		try {
+			System.out.println("Command Interpreter All Primary Commands by Reflection Test" + "\n");
+			
+            Class<?> cls = CommandInterpreterModel.class;
+            Method[] method = cls.getDeclaredMethods();
+            
+            for (int index = 0; index < method.length; index++) {
+
+            	// Commands needs to be all lowercase in order to match the method names
+            	String executionMessage = controller.executeCommand(method[index].getName());
+            	
+            	System.out.println("\n" + executionMessage + "\n");
+            }
+        } 
+		catch (Throwable e) {
+			System.out.println("Command Interpreter All Primary Commands by Reflection Test failed." + "\n");
+        }
+	}
+	
+	private void executeSynonymCommand() {
+		
+		System.out.println("Command Interpreter Synonym Command Test: Amble" + "\n");
+		
+		// Commands needs to be all lowercase in order to match the method names
+		String executionMessage = controller.executeCommand("amble");
+		
+		System.out.println("\n" + executionMessage + "\n");
+		
+		System.out.println("Command Interpreter Synonym Command Test: List" + "\n");
+		
+		// Commands needs to be all lowercase in order to match the method names
+		executionMessage = controller.executeCommand("list");
+		
+		System.out.println(executionMessage);
+	}
+
+	private void executeAllSynonymCommands() {
+
+		List<CommandModel> primaryCommandList = controller.getSynonymCommands();
+		
+		System.out.println("Command Interpreter All Synonym Commands Test" + "\n");
+		
+		for (CommandModel commandModel : primaryCommandList) {
+	
+			// Commands needs to be all lowercase in order to match the method names
+			String executionMessage = controller.executeCommand(commandModel.getCommandName());
+			
+			System.out.println("\n" + executionMessage + "\n");
+        }
+	}
+	
+	/*
+	 * main Method
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		TestCommandInterpreter testCommandInterpreter = new TestCommandInterpreter();
+		testCommandInterpreter.menu();
+	}
+}
